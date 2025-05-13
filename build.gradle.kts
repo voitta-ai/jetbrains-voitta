0a67c2e54c99c298547e951d85ee4c33859f6787
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-05-13 16:16:20 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "1.9.24"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
+    kotlin("plugin.serialization") version "1.9.24"
+}
+
+group = "org.jetbrains"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("IC", "2024.3")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+        plugin("com.intellij.mcpServer", "1.0.30")
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+}
+
+// this has to be compileOnly otherwise there is class collision for kotlinx serialization
+// from the main plugin and the current one
+dependencies {
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "243"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
---
+++ src/main/kotlin/org/jetbrains/mcpextensiondemo/MyCustomTool.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.mcpextensiondemo
+
+import com.intellij.openapi.project.Project
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.jetbrains.ide.mcp.Response
+import org.jetbrains.mcpserverplugin.AbstractMcpTool
+
+class MyCustomTool : AbstractMcpTool<MyArgs>(MyArgs.serializer()) {
+    override val name: String = "my_custom_tool"
+    override val description: String = "Custom tool for the demonstration"
+
+    override fun handle(project: Project, args: MyArgs): Response {
+        return Response("Everything is fine, passed args: ${args.param1}, args: ${args.param2}")
+    }
+}
+
+// Define your arguments data class
+@Serializable
+data class MyArgs(
+    val param1: String,
+    val param2: Int
+)
